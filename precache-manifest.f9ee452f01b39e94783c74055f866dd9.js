c454ff5737906d22a0ef1f16c393441082e51d60
Daniel <d_s_lima@outlook.com>
2020-10-15 11:01:50 -0300

    aodaiud

---
+++ precache-manifest.f9ee452f01b39e94783c74055f866dd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f86a911d9a1e5a7503171161cab4328",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a009785dc2d5babf7b0",
+    "url": "/static/css/2.defa3ad7.chunk.css"
+  },
+  {
+    "revision": "6a009785dc2d5babf7b0",
+    "url": "/static/js/2.51a33955.chunk.js"
+  },
+  {
+    "revision": "cfcd6bb9d3632198657976524f1b10e2",
+    "url": "/static/js/2.51a33955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de5dede50277aa2c895",
+    "url": "/static/js/main.8dca67df.chunk.js"
+  },
+  {
+    "revision": "6f3867a69abeb412d899",
+    "url": "/static/js/runtime-main.329851cd.js"
+  }
+]);
